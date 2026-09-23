5c533281633be4abbffc6862e70fa5767b546847
lammsann <ihsanghulam44@gmail.com>
2026-09-23 07:43:41 +0700

    feat: Add Report

---
+++ sysinfo.sh
@@ -0,0 +1,24 @@
+# Ambil Output
+read -r disk_status disk_detail mem_status mem_detail <<< $(echo "$disk_usage $mem_usage" | ./resource_check)
+
+# Remove (_)
+disk_detail="${disk_detail//_/ }"
+mem_detail="${mem_detail//_/ }"
+
+# File Report
+cat <<EOF > sysinfo_report.txt
+====================================================================
+TUGAS 1 OS - KELOMPOK B07
+====================================================================
+| Check Category | Item               | Status | Details
+--------------------------------------------------------------------
+| OS             | $os_info | PASS   | Kernel $kernel_info
+| Users          | Regular accounts   | PASS   | $user_count akun
+| Processes      | Running            | PASS   | $proc_count proses berjalan
+| Virtualization | Hypervisor         | PASS   | $virt_display
+--------------------------------------------------------------------
+| Disk           | $disk_usage%                | $disk_status   | $disk_detail
+| Memori         | $mem_usage%                | $mem_status   | $mem_detail
+| Uptime         | VM Uptime Check    | PASS   | $uptime_info
+====================================================================
+EOF
