9bc5ce0fdb4dfb6a80cba04128148d0863f7c097
ohdir <hmwith5ms@gmail.com>
2026-09-23 10:11:33 +0700

    feat: Add snapshot functionality and function filtering

--- vm_ctl.sh
+++ vm_ctl.sh
@@ -3,3 +3,24 @@ vm_list() {
     echo "Daftar VM terdaftar:"
     VBoxManage list vms | awk -F'"' '{print NR". "$2}'
 }
+snapshot(){
+    if [ "$2" == "create" ]; then
+        VBoxManage snapshot $3 take $4
+    elif [ "$2" == "list" ]; then
+        VBoxManage snapshot $3 list
+    fi
+}
+echo "===================================
+     TUGAS 1 OS - KELOMPOK B7      
+==================================="
+if [ "$1" == "list" ]; then
+    list $1
+elif [ "$1" == "info" ]; then
+    info $1 $2
+elif [ "$1" == "start" ]; then
+    start $1 $2 
+elif [ "$1" == "stop" ]; then
+    stop $1 $2
+elif [ "$1" == "snapshot" ]; then
+    snapshot $1 $2 $3 $4
+fi
